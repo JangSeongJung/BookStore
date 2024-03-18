6a7680546ac7efed738cf12d86f27c509dca2b08
JangSeongJung <wbrkrtp@naver.com>
2024-03-18 14:41:13 +0900

    북스토어 1차수정

--- src/BookRopository.java
+++ src/BookRopository.java
@@ -1,32 +1,70 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 
 public class BookRopository {
 
     ArrayList<Book> BookList = new ArrayList<>();
 
-    ArrayList books = new ArrayList<Book>();
+    Book book0 = new Book(0,"title","-------------책 제목-------------", "저자이름", "--출판사--", 0000, 00);
+    Book book1 = new Book(1,"파이썬","파이썬을 배워라! 당신의 파이가 늘어난다.", "한용영", "한영출판사", 2012, 5);
+    Book book2 = new Book(2, "프로그래밍","취미로 한 프로그래밍, 나의 주수입원이 되다.", "김미래", "나노출판사", 2017, 12);
+    Book book3 = new Book(3, "자바","자바야 걱정마 내가 너를 자바줄게", "김미래", "날아라출판사", 2011, 3);
+
     public void newBook(Book info){
         BookList.add(info);
     }
     public void AllBook(){
-        for(int i=0; i<BookList.size(); i++){
+        for(int i=1; i<BookList.size(); i++){
             BookList.get(i).look();
         }
     }
     public void FindBookByNum(int bN){
+        int count=0;
         for(int i=0; i<BookList.size(); i++){
             if(BookList.get(i).index==bN){
                 BookList.get(i).look();
+                count++;
             }
         }
+        if(count==0){
+            System.out.println("검색하신 번호의 책이 존재하지 않습니다.");
+        }
     }
-    public void FindBookByName(String name1){
+    public void FindBookByTitle(String title1){
+        int count=0;
         for(int i=0; i<BookList.size(); i++){
-            if(BookList.get(i).name==name1){
+            if(BookList.get(i).title.equals(title1)){
                 BookList.get(i).look();
+                count++;
             }
         }
+        if(count==0){
+            System.out.println("검색하신 타이틀의 책이 존재하지 않습니다.");
+        }
     }
-
+    public void FindBookByName(String name1){ // 이름의 경우 띄어쓰기가 있어서 코딩이 에러가 난다.
+        int count=0;
+        for(int i=0; i<BookList.size(); i++){
+            if(BookList.get(i).name.equals(name1)){
+                BookList.get(i).look();
+                count++;
+            }
+        }
+        if(count==0){
+            System.out.println("검색하신 이름의 책이 존재하지 않습니다.");
+        }
+    }
+    public void FindBookByWriter(String writer1){
+        int count=0;
+        for(int i=0; i<BookList.size(); i++){
+            if(BookList.get(i).writer.equals(writer1)){
+                BookList.get(i).look();
+                count++;
+            }
+        }
+        if(count==0){
+            System.out.println("검색하신 저자의 책이 존재하지 않습니다.");
+        }
+    }
 
 }
--- src/BookStore.java
+++ src/BookStore.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -6,26 +7,75 @@ public class BookStore {
         Scanner SC = new Scanner(System.in);
         BookRopository br1 =new BookRopository();
 
-        Book book1 = new Book(1,"파이썬을 배워라! 당신의 파이가 늘어난다.", "한용영", "한영출판사", 2012, 5);
-        Book book2 = new Book(2, "취미로 한 프로그래밍, 나의 주수입원이 되다.", "김미래", "나노출판사", 2017, 12);
-        Book book3 = new Book(3, "자바야 걱정마 내가 너를 자바줄게", "오아람", "날아라출판사", 2011, 3);
 
-        br1.newBook(book1);
-        br1.newBook(book2);
-        br1.newBook(book3);
+        br1.newBook(br1.book0);
+        br1.newBook(br1.book1);
+        br1.newBook(br1.book2);
+        br1.newBook(br1.book3);
 
-        br1.AllBook();
+        for(;;){
+            int endpro=0;
+            System.out.println("다음 메뉴 중 고르시오.");
+            System.out.println("1.책목록보기 2.번호로책찾기 3.타이틀로책찾기 4.지은이로책찾기 5.책추가하기 0.종료");
+            int sel1=SC.nextInt();
+            switch (sel1){
+                case(1):
+                    System.out.println("현재 책 명단");
+                    br1.AllBook();
+                    break;
+                case(2):
+                    // 번호로 책 찾아내기
+                    System.out.println("검색할 책의 번호를 입력하시오.");
+                    int bN = SC.nextInt();
+                    br1.FindBookByNum(bN);
+                    break;
+                case(3):
+                    // 타이틀로 책 찾아내기
+                    System.out.println("검색할 책의 타이틀명을 입력하시오.");
+                    String title1 = SC.next();
+                    br1.FindBookByTitle(title1);
+                    break;
+                case(4):
+                    // 저자의 이름으로 책 찾아내기
+                    System.out.println("검색할 책의 저자 이름을 입력하시오.");
+                    String writer1 = SC.next();
+                    br1.FindBookByWriter(writer1);
+                    break;
+                case(5):
+                    // 책을 추가하기
+                    AddBook(br1,br1.BookList);
+                    break;
+                case(0):
+                    endpro=1;
+                    break;
+                default:
+                    System.out.println("메뉴에 있는 번호를 입력해 주십시오!");
+            }
+            if(endpro==1){
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+        }
+    } // main end---------------------------------------------------------------------------------------------------
+    public static void AddBook(BookRopository br1,ArrayList<Book> BookList) {
+        Scanner SC = new Scanner(System.in);
+        int howmanybooks = BookList.size() - 1;
+        int index = howmanybooks + 1; // 책 번호
+        System.out.println("타이틀을 입력하시오");
+        String title = SC.next();
+        System.out.println("책의 이름을 입력하시오");
+        String name = SC.next();
+        System.out.println("책의 저자 이름을 입력하시오");
+        String writer = SC.next();
+        System.out.println("출판사를 입력하시오");
+        String cor = SC.next();
+        System.out.println("출판년도를 입력하시오");
+        int year = SC.nextInt();
+        System.out.println("출판월을 입력하시오");
+        int month = SC.nextInt();
 
-        // 번호로 책 찾아내기
-        System.out.println("검색할 책의 번호를 입력하시오.");
-
-        int bN = SC.nextInt();
-        br1.FindBookByNum(bN);
-
-        // 이름으로 책 찾아내기
-        System.out.println("검색할 책의 이름을 입력하시오.");
-        String name1 = SC.next();
-        br1.FindBookByName(name1);
-
+        Book nbook=new Book(index,title ,name,writer,cor,year,month);
+        br1.newBook(nbook);
+        System.out.println(index+"번으로 "+name+"이 추가되었습니다.");
     }
 }
